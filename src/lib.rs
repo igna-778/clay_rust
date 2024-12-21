ee491d6a1f1456d9018e18a98f6f1936dfe31ab8
Mathys-Gasnier <mathys35.gasnier@gmail.com>
2024-12-21 15:47:15 +0100

    Improving build script

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#[cfg(feature = "build-clay")]
+pub mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+#[cfg(not(feature = "build-clay"))]
 pub mod bindings;
 
 pub mod layout;
