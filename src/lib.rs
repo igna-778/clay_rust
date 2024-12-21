807c3a3ebcab5f772c90f4b801e2a6aaedec67e5
Sidney Millen <sidneymillen1@gmail.com>
2024-12-20 20:30:51 -0600

    refactor that may break everything idk yet (I will simply revert this if it does)

--- src/lib.rs
+++ src/lib.rs
@@ -8,155 +8,61 @@ use std::os::raw::{c_uchar, c_uint};
 use bindings::*;
 
 // include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+//use this if using the version compiled by the build script. This library is using a copy of bindings.rs in this directory
 
 pub enum ClayElementConfigType {
-    Rectangle,
-    BorderContainer,
-    FloatingContainer,
-    ScrollContainer,
-    Image,
-    Text,
-    Custom,
-}
-
-impl ClayElementConfigType {
-    pub fn as_c_uchar(&self) -> c_uchar {
-        match self {
-            ClayElementConfigType::Rectangle => {
-                Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_RECTANGLE
-            }
-            ClayElementConfigType::BorderContainer => {
-                Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_BORDER_CONTAINER
-            }
-            ClayElementConfigType::FloatingContainer => {
-                Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_FLOATING_CONTAINER
-            }
-            ClayElementConfigType::ScrollContainer => {
-                Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_SCROLL_CONTAINER
-            }
-            ClayElementConfigType::Image => Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_IMAGE,
-            ClayElementConfigType::Text => Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_TEXT,
-            ClayElementConfigType::Custom => {
-                Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_CUSTOM
-            }
-        }
-    }
+    Rectangle = Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_RECTANGLE as isize,
+    BorderContainer = Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_BORDER_CONTAINER as isize,
+    FloatingContainer =
+        Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_FLOATING_CONTAINER as isize,
+    ScrollContainer = Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_SCROLL_CONTAINER as isize,
+    Image = Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_IMAGE as isize,
+    Text = Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_TEXT as isize,
+    Custom = Clay__ElementConfigType_CLAY__ELEMENT_CONFIG_TYPE_CUSTOM as isize,
 }
 
 pub enum ClayLayoutDirection {
-    LeftToRight,
-    TopToBottom,
-}
-
-impl ClayLayoutDirection {
-    pub fn as_c_uchar(&self) -> c_uchar {
-        match self {
-            ClayLayoutDirection::LeftToRight => Clay_LayoutDirection_CLAY_LEFT_TO_RIGHT,
-            ClayLayoutDirection::TopToBottom => Clay_LayoutDirection_CLAY_TOP_TO_BOTTOM,
-        }
-    }
+    LeftToRight = Clay_LayoutDirection_CLAY_LEFT_TO_RIGHT as isize,
+    TopToBottom = Clay_LayoutDirection_CLAY_TOP_TO_BOTTOM as isize,
 }
 
 pub enum ClayLayoutAlignmentX {
-    Left,
-    Center,
-    Right,
+    Left = Clay_LayoutAlignmentX_CLAY_ALIGN_X_LEFT as isize,
+    Center = Clay_LayoutAlignmentX_CLAY_ALIGN_X_CENTER as isize,
+    Right = Clay_LayoutAlignmentX_CLAY_ALIGN_X_RIGHT as isize,
 }
 
-impl ClayLayoutAlignmentX {
-    pub fn as_c_uchar(&self) -> c_uchar {
-        match self {
-            ClayLayoutAlignmentX::Left => Clay_LayoutAlignmentX_CLAY_ALIGN_X_LEFT,
-            ClayLayoutAlignmentX::Center => Clay_LayoutAlignmentX_CLAY_ALIGN_X_CENTER,
-            ClayLayoutAlignmentX::Right => Clay_LayoutAlignmentX_CLAY_ALIGN_X_RIGHT,
-        }
-    }
-}
 pub enum ClayLayoutAlignmentY {
-    Top,
-    Center,
-    Bottom,
-}
-
-impl ClayLayoutAlignmentY {
-    pub fn as_c_uchar(&self) -> c_uchar {
-        match self {
-            ClayLayoutAlignmentY::Top => Clay_LayoutAlignmentY_CLAY_ALIGN_Y_TOP,
-            ClayLayoutAlignmentY::Center => Clay_LayoutAlignmentY_CLAY_ALIGN_Y_CENTER,
-            ClayLayoutAlignmentY::Bottom => Clay_LayoutAlignmentY_CLAY_ALIGN_Y_BOTTOM,
-        }
-    }
+    Top = Clay_LayoutAlignmentY_CLAY_ALIGN_Y_TOP as isize,
+    Center = Clay_LayoutAlignmentY_CLAY_ALIGN_Y_CENTER as isize,
+    Bottom = Clay_LayoutAlignmentY_CLAY_ALIGN_Y_BOTTOM as isize,
 }
 
 pub enum ClaySizingType {
-    Fit,
-    Grow,
-    Percent,
-    Fixed,
-}
-
-impl ClaySizingType {
-    pub fn as_c_uchar(&self) -> c_uchar {
-        match self {
-            ClaySizingType::Fit => Clay__SizingType_CLAY__SIZING_TYPE_FIT,
-            ClaySizingType::Grow => Clay__SizingType_CLAY__SIZING_TYPE_GROW,
-            ClaySizingType::Percent => Clay__SizingType_CLAY__SIZING_TYPE_PERCENT,
-            ClaySizingType::Fixed => Clay__SizingType_CLAY__SIZING_TYPE_FIXED,
-        }
-    }
+    Fit = Clay__SizingType_CLAY__SIZING_TYPE_FIT as isize,
+    Grow = Clay__SizingType_CLAY__SIZING_TYPE_GROW as isize,
+    Percent = Clay__SizingType_CLAY__SIZING_TYPE_PERCENT as isize,
+    Fixed = Clay__SizingType_CLAY__SIZING_TYPE_FIXED as isize,
 }
 
 pub enum ClayTextElementConfigWrapMode {
-    Words,
-    Newline,
-    None,
-}
-
-impl ClayTextElementConfigWrapMode {
-    pub fn as_c_uint(&self) -> c_uint {
-        match self {
-            ClayTextElementConfigWrapMode::Words => {
-                Clay_TextElementConfigWrapMode_CLAY_TEXT_WRAP_WORDS
-            }
-            ClayTextElementConfigWrapMode::Newline => {
-                Clay_TextElementConfigWrapMode_CLAY_TEXT_WRAP_NEWLINES
-            }
-            ClayTextElementConfigWrapMode::None => {
-                Clay_TextElementConfigWrapMode_CLAY_TEXT_WRAP_NONE
-            }
-        }
-    }
+    Words = Clay_TextElementConfigWrapMode_CLAY_TEXT_WRAP_WORDS as isize,
+    Newline = Clay_TextElementConfigWrapMode_CLAY_TEXT_WRAP_NEWLINES as isize,
+    None = Clay_TextElementConfigWrapMode_CLAY_TEXT_WRAP_NONE as isize,
 }
 
 pub enum ClayRenderCommandType {
-    None,
-    Rectangle,
-    Border,
-    Text,
-    Image,
-    ScissorStart,
-    ScissorEnd,
-    Custom,
+    None = Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_NONE as isize,
+    Rectangle = Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_RECTANGLE as isize,
+    Border = Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_BORDER as isize,
+    Text = Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_TEXT as isize,
+    Image = Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_IMAGE as isize,
+    ScissorStart = Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_SCISSOR_START as isize,
+    ScissorEnd = Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_SCISSOR_END as isize,
+    Custom = Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_CUSTOM as isize,
 }
 
-impl ClayRenderCommandType {
-    pub fn as_c_uint(&self) -> c_uint {
-        match self {
-            ClayRenderCommandType::None => Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_NONE,
-            ClayRenderCommandType::Rectangle => {
-                Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_RECTANGLE
-            }
-            ClayRenderCommandType::Border => Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_BORDER,
-            ClayRenderCommandType::Text => Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_TEXT,
-            ClayRenderCommandType::Image => Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_IMAGE,
-            ClayRenderCommandType::ScissorStart => {
-                Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_SCISSOR_START
-            }
-            ClayRenderCommandType::ScissorEnd => {
-                Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_SCISSOR_END
-            }
-
-            ClayRenderCommandType::Custom => Clay_RenderCommandType_CLAY_RENDER_COMMAND_TYPE_CUSTOM,
-        }
-    }
+pub enum ClayPointerCaptureMode {
+    Capture = Clay_PointerCaptureMode_CLAY_POINTER_CAPTURE_MODE_CAPTURE as isize,
+    Passthrough = Clay_PointerCaptureMode_CLAY_POINTER_CAPTURE_MODE_PASSTHROUGH as isize,
 }
