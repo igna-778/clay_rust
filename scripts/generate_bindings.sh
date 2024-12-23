f35836bb8581a9ed9a93e2129caa2b201f64c1a2
Mathys-Gasnier <mathys35.gasnier@gmail.com>
2024-12-23 18:46:56 +0100

    Adding bindings build instructions and a way to change which clay.h is used

--- scripts/generate_bindings.sh
+++ scripts/generate_bindings.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-if ! command -v bindgen 2>&1 >/dev/null
-then
+if ! command -v bindgen 2>&1 >/dev/null; then
     echo "bindgen not found. Install it using `cargo install bindgen-cli`"
     exit 1
 fi
 
-bindgen clay.h -o src/bindings/bindings.rs
+: "${CLAY_HEADER_PATH:=clay.h}"
+
+bindgen $CLAY_HEADER_PATH -o src/bindings/bindings.rs
 echo "Generated src/bindings/bindings.rs"
-bindgen clay.h -o src/bindings/bindings_debug.rs -- -DCLAY_DEBUG
+bindgen $CLAY_HEADER_PATH -o src/bindings/bindings_debug.rs -- -DCLAY_DEBUG
 echo "Generated src/bindings/bindings_debug.rs"
