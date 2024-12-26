f574ca1943f838d4ebf5807f6c1db6f19faadc01
Daniel Collin <daniel@collin.com>
2024-12-26 10:53:49 +0100

    Windows fixes (#26)

--- build.rs
+++ build.rs
@@ -1,6 +1,15 @@
 fn main() {
-    cc::Build::new()
-        .file("build.c")
-        .extra_warnings(false)
-        .compile("clay");
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    if target_os == "windows" {
+        cc::Build::new()
+            .file("build.cpp")
+            .std("c++20")
+            .compile("clay");
+    } else {
+        cc::Build::new()
+            .file("build.c")
+            .extra_warnings(false)
+            .compile("clay");
+    }
 }
