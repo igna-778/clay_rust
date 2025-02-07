b2a60a0ad965ae0717deb73b79ec1e9ad3564f1f
Daniel Collin <daniel@collin.com>
2025-02-07 18:35:55 +0100

    Added static assert for percent range & updated docs (#53)

--- src/layout.rs
+++ src/layout.rs
@@ -23,7 +23,7 @@ pub enum Sizing {
     Grow(f32, f32),
     /// Sets a fixed width/height.
     Fixed(f32),
-    /// Sets width/height as a percentage of its parent.
+    /// Sets width/height as a percentage of its parent. Value should be between `0.0` and `1.0`.
     Percent(f32),
 }
 
@@ -252,11 +252,16 @@ macro_rules! fixed {
 }
 
 /// Shorthand macro for [`Sizing::Percent`].
+/// The value has to be in range `0.0..=1.0`.
 #[macro_export]
 macro_rules! percent {
-    ($percent:expr) => {
+    ($percent:expr) => {{
+        const _: () = assert!(
+            $percent >= 0.0 && $percent <= 1.0,
+            "Percent value must be between 0.0 and 1.0 inclusive!"
+        );
         $crate::layout::Sizing::Percent($percent)
-    };
+    }};
 }
 
 #[cfg(test)]
